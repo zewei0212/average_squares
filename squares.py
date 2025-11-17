7eddc1c2b5c8913d252fcde052dbf6d474167c8e
zewei0212 <zcapzwh@ucl.ac.uk>
2025-11-17 10:27:26 +0000

    issue 31

--- squares.py
+++ squares.py
@@ -1,5 +1,7 @@
 """Computation of weighted average of squares."""
 
+import argparse 
+
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """Return the weighted average of squares of the given values.
 
@@ -17,6 +19,7 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
     ...
     AssertionError: weights and numbers must have same length
     """
+ 
     if list_of_weights is not None:
         assert len(list_of_weights) == len(list_of_numbers), \
             "weights and numbers must have same length"
@@ -25,8 +28,8 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
         effective_weights = [1] * len(list_of_numbers)
 
     weighted_sum_squares = sum(
-        w * (x * x) for x, w in zip(list_of_numbers, effective_weights) )
-    
+        w * (x * x) for x, w in zip(list_of_numbers, effective_weights)
+    )
     total_weight = sum(effective_weights)
 
     return weighted_sum_squares / total_weight
@@ -40,19 +43,35 @@ def convert_numbers(list_of_strings):
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
     [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
     """
+    
     all_numbers = []
     for s in list_of_strings:
         all_numbers.extend([token.strip() for token in s.split()])
-    
+
     return [float(number_string) for number_string in all_numbers]
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1", "2", "4"]
-    weight_strings = ["1", "1", "1"]
+   
+    parser = argparse.ArgumentParser(
+        description="Compute the (unweighted) average of squares of the given numbers."
+    )
 
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
+# CHange the inputs to float
+    parser.add_argument(
+        "numbers",
+        nargs="+",
+        type=float,
+        help="Numbers for which to compute the average of squares.",
+    )
 
-    result = average_of_squares(numbers, weights)
-    print(result)  
+
+    args = parser.parse_args()
+
+  
+    numbers = args.numbers
+
+    # Assume the weight is 1 for every items
+    result = average_of_squares(numbers)
+
+    print(result)
